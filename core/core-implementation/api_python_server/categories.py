3424e7e613bf653d1c674286868c2c9d2eaa2c83
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2024-06-15 12:29:12 +0200

    Simplified tags to limit number of API files.

---
+++ core/core-implementation/api_python_server/categories.py
@@ -0,0 +1,15 @@
+""" Module categories 
+
+    
+
+    Created on 14/06/2024
+    By Jean-Marc Le Peuvédic
+    © CalCool Studios SAS 2021-2024
+"""
+# TODO List
+
+# Import system libraries
+
+# Import public libraries
+
+# Import application modules
